6246fcd409a2ffd84148acdb5206e97e6df1b3af
Nouran  Ayman  Mohamed <nouraaan.aymaaan@gmail.com>
2020-12-22 22:17:55 +0200

    Level1_Adding Main Filling Logic.

---
+++ DailyRoutineGame/Assets/Scripts/Level(1)/DraggableObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraggableObject : MonoBehaviour
+{
+    public float DragSpeed;
+
+    [Space(20)]
+
+    public float MaxPosX;
+    public float MinPosX;
+
+    [Space(20)]
+
+    public float MaxPosZ;
+    public float MinPosZ;
+
+    Vector3 lastMousePos;
+
+    private void OnMouseDown()
+    {
+        lastMousePos = Input.mousePosition;
+    }
+
+
+    void OnMouseDrag()
+    {
+        Vector3 delta = Input.mousePosition - lastMousePos;
+        Vector3 pos = transform.position;
+
+        pos.x += delta.x * DragSpeed * -1f;
+        pos.z += delta.y * DragSpeed * -1f;
+
+        if ((MinPosX <= pos.x) && (MaxPosX >= pos.x) && (MinPosZ <= pos.z) && (MaxPosZ >= pos.z))
+        {
+            transform.position = pos;
+        }
+        
+
+        lastMousePos = Input.mousePosition;
+    }
+
+}
--- DailyRoutineGame/Assets/Scripts/Level(1)/PancakeLevelManager.cs
+++ DailyRoutineGame/Assets/Scripts/Level(1)/PancakeLevelManager.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Es.InkPainter;
 
 public class PancakeLevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[Header("Filling Attributes")]
+	public GameObject Filler;
+	public ParticleSystem FillingParticle;
+	
+	[Header("Painting Attributes")]
+	[SerializeField]
+	private Brush brush;
+	[SerializeField]
+	LayerMask PaintLayer;
+	RaycastHit hit;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			FillingParticle.Play();
+		}
+
+		else if (Input.GetMouseButton(0))
+		{
+			if (Physics.Raycast(Filler.transform.position, -Vector3.up, out hit, 50, PaintLayer))
+			{
+				var paintObject = hit.transform.GetComponent<InkCanvas>();
+
+				if (paintObject != null)
+					paintObject.Paint(brush, hit);
+
+			}
+		}
+
+		else if (Input.GetMouseButtonUp(0))
+		{
+			FillingParticle.Stop();
+		}
+	}
 }
