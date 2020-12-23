1b22fcc7e2723e875dbefa74794d0584b4a0d732
Nouran  Ayman  Mohamed <nouraaan.aymaaan@gmail.com>
2020-12-23 16:02:34 +0200

    Importing SweetCan model and Add Sweeting main logic.

---
+++ DailyRoutineGame/Assets/Scripts/Level(1)/Sweeter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sweeter : MonoBehaviour
+{
+    public GameObject SweetsPrefab;
+    public bool CanClick = true;
+
+    public Transform[] SpawnPoints;
+    int randomPoint;
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0) && CanClick)
+        {
+            Debug.Log("Click on sweeter !");
+            
+            randomPoint = Random.Range(0, SpawnPoints.Length);
+
+            Instantiate(SweetsPrefab, SpawnPoints[randomPoint].position, Quaternion.identity);
+        }
+    }
+
+}
